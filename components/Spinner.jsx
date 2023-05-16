7b7e80a2b371ecff6b546c068987964c6fc27ae1
rosmildoLopes <[200~123316625+rosmildoLopes@users.noreply.github.com~>
2023-05-16 20:49:04 -0300

    first commit

---
+++ components/Spinner.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import spinner from "../public/spinner.gif"
+import Image from 'next/image'
+
+const Spinner = () => {
+  return (
+    <>
+      <Image 
+      className="w-[200px] m-auto block"
+      src={spinner} alt='Loading...' />
+    </>
+  )
+}
+
+export default Spinner
